fab24290978632e3b1c74afae7517d2db91f84d4
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-05-03 11:37:24 +0200

    tests

---
+++ tests/Pulumi.Azure.Extensions.Tests/Storage/BlobCollectionTests.cs
@@ -0,0 +1,76 @@
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Pulumi.Azure.Constants;
+using Pulumi.Azure.Extensions.Storage;
+using Xunit;
+
+namespace Pulumi.Azure.Extensions.Tests.Storage
+{
+    public class BlobCollectionTests
+    {
+        private const string BlobCollectionName = "test";
+        private static string FilesFolder => Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName, "netcoreapp3.1", "files");
+
+        private class BlobCollectionStackFolderNoFiles : Stack
+        {
+            public BlobCollectionStackFolderNoFiles()
+            {
+                var args = new BlobCollectionArgs
+                {
+                    Source = Path.Combine(FilesFolder, "y"),
+                    StorageAccountName = "sa",
+                    StorageContainerName = "sc",
+                    Type = BlobTypes.Block
+                };
+
+                _ = new BlobCollection(BlobCollectionName, args);
+            }
+        }
+
+        private class BlobCollectionStackEmptyFile : Stack
+        {
+            public BlobCollectionStackEmptyFile()
+            {
+                var args = new BlobCollectionArgs
+                {
+                    Source = Path.Combine(FilesFolder, "y", "0.txt"),
+                    StorageAccountName = "sa",
+                    StorageContainerName = "sc",
+                    Type = BlobTypes.Block
+                };
+
+                _ = new BlobCollection(BlobCollectionName, args);
+            }
+        }
+
+        [Fact]
+        public async Task Folder_NoFiles()
+        {
+            // Arrange and Act
+            var resources = await Testing.RunAsync<BlobCollectionStackFolderNoFiles>();
+
+            // Assert
+            resources.Length.Should().Be(2);
+            var blobCollection = resources.OfType<BlobCollection>().FirstOrDefault();
+
+            Assert.NotNull(blobCollection);
+            blobCollection.GetResourceName().Should().Be("test");
+        }
+
+        [Fact]
+        public async Task EmptyFile()
+        {
+            // Arrange and Act
+            var resources = await Testing.RunAsync<BlobCollectionStackEmptyFile>();
+
+            // Assert
+            resources.Length.Should().Be(2);
+            var blobCollection = resources.OfType<BlobCollection>().FirstOrDefault();
+
+            Assert.NotNull(blobCollection);
+            blobCollection.GetResourceName().Should().Be("test");
+        }
+    }
+}
---
+++ tests/Pulumi.Azure.Extensions.Tests/Testing.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Immutable;
+using System.Threading.Tasks;
+using Moq;
+using Pulumi.Testing;
+
+namespace Pulumi.Azure.Extensions.Tests
+{
+    public static class Testing
+    {
+        public static Task<ImmutableArray<Resource>> RunAsync<T>() where T : Stack, new()
+        {
+            var mocks = new Mock<IMocks>();
+
+            mocks.Setup(m => m.NewResourceAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<ImmutableDictionary<string, object>>(), It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync((string type, string name, ImmutableDictionary<string, object> inputs, string? provider, string? id) => (id ?? "", inputs));
+            
+            mocks.Setup(m => m.CallAsync(It.IsAny<string>(), It.IsAny<ImmutableDictionary<string, object>>(), It.IsAny<string>()))
+                .ReturnsAsync((string token, ImmutableDictionary<string, object> args, string? provider) => args);
+            
+            return Deployment.TestAsync<T>(mocks.Object, new TestOptions { IsPreview = false });
+        }
+    }
+}
