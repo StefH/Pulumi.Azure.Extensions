38448376cbed2cd5f1f3ae5cb6238a00f9f51130
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-05-04 14:24:53 +0000

    .

--- tests/Pulumi.Azure.Extensions.Tests/Storage/BlobCollectionTests.cs
+++ tests/Pulumi.Azure.Extensions.Tests/Storage/BlobCollectionTests.cs
@@ -14,28 +14,28 @@ namespace Pulumi.Azure.Extensions.Tests.Storage
         private const string BlobCollectionName = "test";
         private static string FilesFolder => Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName, "netcoreapp3.1", "files");
 
-private class BlobStack : Stack
-{
-    public BlobStack()
-    {
-        var blobArgs = new BlobArgs
+        private class BlobStack : Stack
         {
-            AccessTier = BlobAccessTiers.Hot,
-            Name = "test",
-            Source = new FileAsset(Path.Combine(FilesFolder, "TextFile1.txt")),
-            StorageAccountName = "sa",
-            StorageContainerName = "sa",
-            Type = BlobTypes.Block
-        };
+            public BlobStack()
+            {
+                var blobArgs = new BlobArgs
+                {
+                    AccessTier = BlobAccessTiers.Hot,
+                    Name = "test",
+                    Source = new FileAsset(Path.Combine(FilesFolder, "TextFile1.txt")),
+                    StorageAccountName = "sa",
+                    StorageContainerName = "sa",
+                    Type = BlobTypes.Block
+                };
 
-        var blobOptions = new CustomResourceOptions
-        {
-            Parent = this
-        };
+                var blobOptions = new CustomResourceOptions
+                {
+                    Parent = this
+                };
 
-        _ = new Blob("test", blobArgs, blobOptions);
-    }
-}
+                _ = new Blob("test", blobArgs, blobOptions);
+            }
+        }
 
         private class BlobCollectionStackFolderNoFiles : Stack
         {
@@ -44,6 +44,22 @@ private class BlobStack : Stack
                 var args = new BlobCollectionArgs
                 {
                     Source = Path.Combine(FilesFolder, "y"),
+                    StorageAccountName = "sa",
+                    StorageContainerName = "sc",
+                    Type = BlobTypes.Block
+                };
+
+                _ = new BlobCollection(BlobCollectionName, args);
+            }
+        }
+
+        private class BlobCollectionStackFolderWithFiles : Stack
+        {
+            public BlobCollectionStackFolderWithFiles()
+            {
+                var args = new BlobCollectionArgs
+                {
+                    Source = FilesFolder,
                     StorageAccountName = "sa",
                     StorageContainerName = "sc",
                     Type = BlobTypes.Block
@@ -85,22 +101,22 @@ private class BlobStack : Stack
             }
         }
 
-[Fact]
-public async Task Blob()
-{
-    // Arrange and Act
-    var resources = await Testing.RunAsync<BlobStack>();
+        [Fact]
+        public async Task Blob()
+        {
+            // Arrange and Act
+            var resources = await Testing.RunAsync<BlobStack>();
 
-    // Assert
-    resources.Length.Should().Be(2);
-    var blob = resources.OfType<Blob>().FirstOrDefault();
+            // Assert
+            resources.Length.Should().Be(2);
+            var blob = resources.OfType<Blob>().FirstOrDefault();
 
-    Assert.NotNull(blob);
-    blob.GetResourceName().Should().Be("test");
-}
+            Assert.NotNull(blob);
+            blob.GetResourceName().Should().Be("test");
+        }
 
         [Fact]
-        public async Task Folder_NoFiles()
+        public async Task Folder_WithEmptyFile()
         {
             // Arrange and Act
             var resources = await Testing.RunAsync<BlobCollectionStackFolderNoFiles>();
@@ -111,6 +127,25 @@ public async Task Blob()
 
             Assert.NotNull(blobCollection);
             blobCollection.GetResourceName().Should().Be("test");
+        }
+
+        [Fact]
+        public async Task Folder_WithFiles()
+        {
+            // Arrange and Act
+            var resources = await Testing.RunAsync<BlobCollectionStackFolderWithFiles>();
+
+            // Assert
+            resources.Length.Should().Be(6);
+            var blobCollection = resources.OfType<BlobCollection>().FirstOrDefault();
+
+            Assert.NotNull(blobCollection);
+            blobCollection.GetResourceName().Should().Be("test");
+
+            var blobs = resources.OfType<Blob>().ToList();
+            Assert.NotNull(blobs);
+
+            blobs.Count.Should().Be(4);
         }
 
         [Fact]
--- tests/Pulumi.Azure.Extensions.Tests/Testing.cs
+++ tests/Pulumi.Azure.Extensions.Tests/Testing.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Immutable;
+﻿using System;
+using System.Collections.Immutable;
 using System.Threading.Tasks;
 using Moq;
 using Pulumi.Testing;
@@ -12,12 +13,34 @@ namespace Pulumi.Azure.Extensions.Tests
             var mocks = new Mock<IMocks>();
 
             mocks.Setup(m => m.NewResourceAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<ImmutableDictionary<string, object>>(), It.IsAny<string>(), It.IsAny<string>()))
-                .ReturnsAsync((string type, string name, ImmutableDictionary<string, object> inputs, string? provider, string? id) => (id ?? "", inputs));
-            
+                .ReturnsAsync((string type, string name, ImmutableDictionary<string, object> inputs, string? provider, string? id) => (id ?? Guid.NewGuid().ToString(), Convert(inputs)));
+
             mocks.Setup(m => m.CallAsync(It.IsAny<string>(), It.IsAny<ImmutableDictionary<string, object>>(), It.IsAny<string>()))
                 .ReturnsAsync((string token, ImmutableDictionary<string, object> args, string? provider) => args);
-            
+
             return Deployment.TestAsync<T>(mocks.Object, new TestOptions { IsPreview = false });
+        }
+
+        private static ImmutableDictionary<string, object> Convert(ImmutableDictionary<string, object> dictionary)
+        {
+            var builder = ImmutableDictionary.CreateBuilder<string, object>();
+            foreach (var entry in dictionary)
+            {
+                // https://github.com/pulumi/pulumi/issues/4558
+                switch (entry.Value)
+                {
+                    case int _:
+                    case double _:
+                    case bool _:
+                    case string s:
+                    case ImmutableArray<object> _:
+                    case ImmutableDictionary<string, object> _:
+                        builder.Add(entry.Key, entry.Value);
+                        break;
+                }
+            }
+
+            return builder.ToImmutable();
         }
     }
 }
